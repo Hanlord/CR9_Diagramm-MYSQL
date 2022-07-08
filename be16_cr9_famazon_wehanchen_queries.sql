14ba5e928f4a7a170f66fe064ae993cb8dbbc238
We-Han <chen007@live.at>
2022-07-09 01:13:53 +0200

    query update

--- be16_cr9_famazon_wehanchen_queries.sql
+++ be16_cr9_famazon_wehanchen_queries.sql
@@ -10,20 +10,28 @@ FROM `office`
 	LEFT JOIN `employer` ON `office`.`fk_employer_id` = `employer`.`id`;
 
 -- 3.query
-SELECT `office`.`fk_employer_id`, `employer`.`f_name`, `employer`.`salary`
-FROM `office` 
-	LEFT JOIN `employer` ON `office`.`fk_employer_id` = `employer`.`id`;
+SELECT `customer`.`l_name`, `order`.`order_date`, `many_product`.`fk_product_id`, `product`.`product_price`
+FROM `customer` 
+	LEFT JOIN `order` ON `order`.`fk_customer_id` = `customer`.`id`
+	, `many_product` 
+	LEFT JOIN `product` ON `many_product`.`fk_product_id` = `product`.`id`;
 
 -- 4.query
-SELECT `order`.`payment`, `customer`.`f_name`, `customer`.`l_name`
-FROM `order` 
-	LEFT JOIN `customer` ON `order`.`fk_customer_id` = `customer`.`id`;
+SELECT `customer`.`l_name`, `order`.`payment`, `many_product`.`fk_company_id`, `company`.`name`, `shipping`.`fk_many_product_id`
+FROM `customer` 
+	LEFT JOIN `order` ON `order`.`fk_customer_id` = `customer`.`id`
+	, `many_product` 
+	LEFT JOIN `company` ON `many_product`.`fk_company_id` = `company`.`id` 
+	LEFT JOIN `shipping` ON `shipping`.`fk_many_product_id` = `many_product`.`id`;
 
 -- 5.query
-SELECT `order`.`order_date`, `product`.`product_type`, `customer`.`email`
-FROM `order`
+SELECT `customer`.`l_name`, `product`.`product_price`, `many_product`.`fk_product_id`, `e-commerce`.`fk_order_id`, `order`.`payment`, `shipping`.`fk_many_product_id`, `product`.`product_type`
+FROM `customer`
 	, `product` 
-	LEFT JOIN `customer` ON `order`.`fk_customer_id` = `customer`.`id`;
+	LEFT JOIN `many_product` ON `many_product`.`fk_product_id` = `product`.`id` 
+	LEFT JOIN `e-commerce` ON `e-commerce`.`fk_many_product_id` = `many_product`.`id` 
+	LEFT JOIN `order` ON `e-commerce`.`fk_order_id` = `order`.`id` 
+	LEFT JOIN `shipping` ON `shipping`.`fk_many_product_id` = `many_product`.`id`;
 
 -- 6.query
 SELECT `product`.`product_name`, `stock`.`quantity`, `product`.`product_price`
